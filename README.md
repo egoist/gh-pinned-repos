8f98bff27afdf9b1cefe4fe588c81a646d4316cb
EGOIST <0x142857@gmail.com>
2016-11-15 17:00:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# gh-pinned-repos
+
+https://gh-pinned-repos.now.sh
+
+## License
+
+[MIT](https://egoist.mit-license.org/) © [EGOIST](https://github.com/egoist)
---
+++ index.js
@@ -0,0 +1,68 @@
+const url = require('url')
+const qs = require('querystring')
+const aimer = require('aimer')
+const {send} = require('micro')
+
+const cache = require('lru-cache')({
+  max: 1024 * 1024,
+  maxAge: 86400
+})
+
+function ghPinnedRepos(username) {
+  return aimer(`https://github.com/${username}`)
+    .then($ => {
+      const pinned = $('.pinned-repo-item.public')
+      if (!pinned || pinned.length === 0) return []
+
+      const result = []
+      pinned.each((index, item) => {
+        const lanaguage = $(item).find('p.mb-0').contents().get(2).data.trim()
+        const forks = $(item).find('a[href$="/network"]').text().trim()
+        result[index] = {
+          repo: $(item).find('.repo').text(),
+          owner: username,
+          description: $(item).find('.pinned-repo-desc').html().trim(),
+          lanaguage: lanaguage || undefined,
+          stars: $(item).find('a[href$="/stargazers"]').text().trim(),
+          forks: forks ? forks : 0
+        }
+      })
+      return result
+    })
+}
+
+module.exports = async function (req, res) {
+  const {pathname, query} = url.parse(req.url)
+  const username = qs.parse(query).username
+
+  if (pathname === '/favicon.ico') {
+    return send(res, 404, '')
+  }
+
+  if (!username) {
+    res.setHeader('Content-Type', 'text/html')
+    return send(res, 200, `
+  <style>body {font-family: Helvetica, serif;margin: 30px;}</style>
+  <p>GET /?username=GITHUB_USERNAME</p>
+
+  <p>
+    <form action="/">
+      <input type="text" name="username" placeholder="username" />
+      <button type="submit">Go!</button>
+    </form>
+  </p>
+
+  <p>made by <a href="https://github.com/egoist">@egoist</a></p>
+`)
+  }
+
+  let result
+  const cachedResult = cache.get(username)
+  if (cachedResult) {
+    result = cachedResult
+  } else {
+    result = await ghPinnedRepos(username)
+    cache.set(username, result)
+  }
+  send(res, 200, result)
+}
