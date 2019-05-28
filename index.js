a9118f681cdd4a5daf8a6cbd39a5c792d223a2b4
EGOIST <0x142857@gmail.com>
2019-05-28 22:30:12 +0800

    tweaks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = require('url')
 const qs = require('querystring')
 const aimer = require('aimer')
-const {send} = require('micro')
+const { send } = require('micro')
 
 const cache = require('lru-cache')({
   max: 1024 * 1024,
@@ -9,34 +9,43 @@ const cache = require('lru-cache')({
 })
 
 function ghPinnedRepos(username) {
-  return aimer(`https://github.com/${username}`)
-    .then($ => {
-      const pinned = $('.pinned-repo-item.public')
-      if (!pinned || pinned.length === 0) return []
+  return aimer(`https://github.com/${username}`).then($ => {
+    const pinned = $('.pinned-item-list-item.public')
+    if (!pinned || pinned.length === 0) return []
 
-      const result = []
-      pinned.each((index, item) => {
-        let language = $(item).find('p.mb-0').contents().get(2)
-        language = language && language.data.trim()
+    const result = []
+    pinned.each((index, item) => {
+      let language = $(item)
+        .find('[itemprop="programmingLanguage"]')
+        .text()
 
-        let owner = $(item).find('.owner').text().trim()
-        owner = owner || username
-
-        const forks = $(item).find('a[href$="/network"]').text().trim()
-        result[index] = {
-          repo: $(item).find('.repo').text(),
-          owner,
-          description: $(item).find('.pinned-repo-desc').html().trim(),
-          language: language || undefined,
-          stars: $(item).find('a[href$="/stargazers"]').text().trim(),
-          forks: forks ? forks : 0
-        }
-      })
-      return result
+      const owner =$(item).find('.owner').text()
+      const repo = $(item).find('.repo').text()
+      const stars = $(item)
+      .find('a[href$="/stargazers"]')
+      .text()
+      .trim()
+      const forks = $(item)
+        .find('a[href$="/network/members"]')
+        .text()
+        .trim()
+      result[index] = {
+        owner: owner || undefined,
+        repo,
+        description: $(item)
+          .find('.pinned-item-desc')
+          .text()
+          .trim(),
+        language: language || undefined,
+        stars: stars || 0,
+        forks: forks || 0
+      }
     })
+    return result
+  })
 }
 
-module.exports = async function (req, res) {
+module.exports = async function(req, res) {
   /* allow cors from any origin */
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Request-Method', '*')
@@ -46,16 +55,15 @@ module.exports = async function (req, res) {
     return send(res, 200)
   }
 
-  const {pathname, query} = url.parse(req.url)
-  const {username, refresh} = qs.parse(query)
-
-  if (pathname === '/favicon.ico') {
-    return send(res, 404, '')
-  }
+  const { query } = url.parse(req.url)
+  const { username, refresh } = qs.parse(query)
 
   if (!username) {
     res.setHeader('Content-Type', 'text/html')
-    return send(res, 200, `
+    return send(
+      res,
+      200,
+      `
   <head>
     <meta charset="utf-8" />
     <title>GitHub pinned repos API</title>
@@ -71,7 +79,8 @@ module.exports = async function (req, res) {
   </p>
 
   <p>made by <a href="https://github.com/egoist">@egoist</a> · <a href="https://github.com/egoist/gh-pinned-repos">source code</a></p>
-`)
+`
+    )
   }
 
   let result
