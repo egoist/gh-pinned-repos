d05f257a810fe15434fb3dab66e57e786866fa5b
ikathuria <ishani@kathuria.net>
2021-09-04 22:58:38 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-const url = require('url')
-const qs = require('querystring')
-const aimer = require('aimer')
-const { send } = require('micro')
+const url = require("url");
+const qs = require("querystring");
+const aimer = require("aimer");
+const { send } = require("micro");
 
-const cache = require('lru-cache')({
+const cache = require("lru-cache")({
   max: 1024 * 1024,
-  maxAge: 86400 * 30
-})
+  maxAge: 86400 * 30,
+});
 
 function ghPinnedRepos(username) {
-  return aimer(`https://github.com/${username}`).then($ => {
-    const pinned = $('.pinned-item-list-item.public')
-    if (!pinned || pinned.length === 0) return []
+  return aimer(`https://github.com/${username}`).then(($) => {
+    const pinned = $(".pinned-item-list-item.public");
+    if (!pinned || pinned.length === 0) return [];
 
-    const result = []
+    const result = [];
     pinned.each((index, item) => {
-
-      const language = getLanguage($, item)
-      const owner = getOwner($, item)
-      const repo = getRepo($, item)
-      const description = getDescription($, item)
-      const stars = getStars($, item)
-      const forks = getForks($, item)
-      const languageColor = getLanguageColor($, item)
+      const language = getLanguage($, item);
+      const owner = getOwner($, item);
+      const repo = getRepo($, item);
+      const description = getDescription($, item);
+      const website = getWebsite($, repo);
+      const stars = getStars($, item);
+      const forks = getForks($, item);
+      const languageColor = getLanguageColor($, item);
 
       result[index] = {
         owner: owner || username,
         repo,
         description: description || undefined,
+        website: website || undefined,
         language: language || undefined,
         languageColor: languageColor || undefined,
         stars: stars || 0,
-        forks: forks || 0
-      }
-    })
-    return result
-  })
+        forks: forks || 0,
+      };
+    });
+    return result;
+  });
 }
 
 function getLanguage($, item) {
   try {
-    return $(item)
-      .find('[itemprop="programmingLanguage"]')
-      .text()
+    return $(item).find('[itemprop="programmingLanguage"]').text();
   } catch (error) {
     return undefined;
   }
@@ -50,9 +49,7 @@ function getLanguage($, item) {
 
 function getOwner($, item) {
   try {
-    return $(item)
-      .find('.owner')
-      .text()
+    return $(item).find(".owner").text();
   } catch (error) {
     return undefined;
   }
@@ -60,9 +57,7 @@ function getOwner($, item) {
 
 function getRepo($, item) {
   try {
-    return $(item)
-      .find('.repo')
-      .text()
+    return $(item).find(".repo").text();
   } catch (error) {
     return undefined;
   }
@@ -70,21 +65,30 @@ function getRepo($, item) {
 
 function getDescription($, item) {
   try {
-    return $(item)
-      .find('.pinned-item-desc')
-      .text()
-      .trim()
+    return $(item).find(".pinned-item-desc").text().trim();
   } catch (error) {
     return undefined;
   }
 }
 
+function getWebsite($, repo) {
+  return aimer(repo).then(($) => {
+    const site = $(".details-reset.details-overlay");
+    if (!pinned || pinned.length === 0) return [];
+
+    site.each((index, item) => {
+      try {
+        return $(item).find(".text-bold").text();
+      } catch (error) {
+        return undefined;
+      }
+    });
+  });
+}
+
 function getStars($, item) {
   try {
-    return parseInt($(item)
-      .find('a[href$="/stargazers"]')
-      .text()
-      .trim())
+    return parseInt($(item).find('a[href$="/stargazers"]').text().trim());
   } catch (error) {
     return 0;
   }
@@ -92,10 +96,7 @@ function getStars($, item) {
 
 function getForks($, item) {
   try {
-    return parseInt($(item)
-      .find('a[href$="/network"]')
-      .text()
-      .trim())
+    return parseInt($(item).find('a[href$="/network"]').text().trim());
   } catch (error) {
     return 0;
   }
@@ -103,31 +104,27 @@ function getForks($, item) {
 
 function getLanguageColor($, item) {
   try {
-    return $(item)
-      .find('.repo-language-color')
-      .css("background-color");
+    return $(item).find(".repo-language-color").css("background-color");
   } catch (error) {
     return undefined;
   }
 }
 
-
-
-module.exports = async function(req, res) {
+module.exports = async function (req, res) {
   /* allow cors from any origin */
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Request-Method', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  if (req.method === 'OPTIONS') {
-    return send(res, 200)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    return send(res, 200);
   }
 
-  const { query } = url.parse(req.url)
-  const { username, refresh } = qs.parse(query)
+  const { query } = url.parse(req.url);
+  const { username, refresh } = qs.parse(query);
 
   if (!username) {
-    res.setHeader('Content-Type', 'text/html')
+    res.setHeader("Content-Type", "text/html");
     return send(
       res,
       200,
@@ -148,16 +145,17 @@ module.exports = async function(req, res) {
 
   <p>made by <a href="https://github.com/egoist">@egoist</a> · <a href="https://github.com/egoist/gh-pinned-repos">source code</a></p>
 `
-    )
+    );
   }
 
-  let result
-  const cachedResult = cache.get(username)
+  let result;
+  const cachedResult = cache.get(username);
   if (cachedResult && !refresh) {
-    result = cachedResult
+    result = cachedResult;
   } else {
-    result = await ghPinnedRepos(username)
-    cache.set(username, JSON.stringify(result))
+    result = await ghPinnedRepos(username);
+    cache.set(username, JSON.stringify(result));
   }
-  send(res, 200, result)
-}
+  console.log(result);
+  send(res, 200, result);
+};
